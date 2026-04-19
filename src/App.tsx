5c307cf2a71b74c77c132720ad4fb80b69242e9b
Yaroslav Shilov <workyarik@gmail.com>
2026-04-19 14:58:47 +0400

    First request

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,87 @@
-import * as React from "react";
 import "./App.css";
-import { useTodoStore } from "./model/todoStore.ts";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const BASEURL = "https://pokeapi.co/api/v2/";
 
 function App() {
-  const { list, addTodo, toggle } = useTodoStore();
-  const [text, setText] = useState("");
-
-  const toggleCheck = (id: (typeof list)[number]["id"]) => () => toggle(id);
-
-  const formHandler = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setText("");
-    addTodo(text);
-  };
-
+  const [state, setState] = useState(false);
   return (
     <div>
-      <form onSubmit={formHandler}>
-        <input
-          type="text"
-          name="task"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button>Create task</button>
-      </form>
-
-      <ul>
-        {list.map(({ id, text, isComplete }) => (
-          <li key={id}>
-            {text}{" "}
-            <input
-              type="checkbox"
-              name={text}
-              checked={isComplete}
-              onChange={toggleCheck(id)}
-            />
-          </li>
-        ))}
-      </ul>
+      <button onClick={() => setState(!state)}>do</button>
+      {state && <Pokemons />}
     </div>
   );
 }
+
+const fetchPokemons = ({
+  takeCount,
+  pageNumber,
+}: {
+  takeCount: number;
+  pageNumber: number;
+}) => {
+  const offset = pageNumber > 1 ? (pageNumber - 1) * takeCount : 0;
+  const url = new URL(BASEURL + "pokemon/");
+  url.searchParams.append("limit", String(takeCount));
+  url.searchParams.append("offset", String(offset));
+
+  return fetch(url)
+    .then((res) => res.json())
+    .then((res) =>
+      res.results.map((item: { name: string; url: string }) => {
+        const id = item.url.split("/").at(-2);
+        console.log("id: ", id);
+
+        return {
+          id,
+          name: item.name,
+          img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`,
+          url: item.url,
+        };
+      }),
+    )
+    .catch((err) => {
+      console.log("err", err);
+    });
+};
+
+const Pokemons = () => {
+  const [state, setState] = useState<{
+    value: { name: string; id: string | number; img: string }[] | null;
+    isLoading: boolean;
+  }>({ value: null, isLoading: true });
+
+  useEffect(() => {
+    fetchPokemons({ takeCount: 10, pageNumber: 1 }).then((res) => {
+      console.log("an: ", res);
+      setState({ value: res, isLoading: false });
+    });
+  }, []);
+
+  return (
+    <div>
+      {state.isLoading && "Loading..."}
+      {!state.isLoading && state.value && (
+        <div>
+          {state.value.map(({ name, id, img }) => (
+            <div key={id}>
+              <p>{name}</p>
+              <p>#{id}</p>
+              <img src={img} alt={name} />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+/*
+type IPokemon = {
+  name: string;
+  url: string;
+  img: string;
+};
+*/
 
 export default App;
--- src/helpers/addTen.ts
+++
@@ -1,7 +0,0 @@
-import { useCounterStore } from "../model/counterStore.ts";
-
-export const addTen = () => {
-  const counter = useCounterStore.getState().counter;
-
-  useCounterStore.getState().changeByAmount(counter > 0 ? 10 : -10);
-};
